624fd1a48f6818961ff7322c5b5e3ad28ab4eb9b
Benjamin Nicholson <benicholson@my.waketech.edu>
2021-11-05 23:16:07 -0400

    added bare-bones function calls for testing purposes

--- student.py
+++ student.py
@@ -1,1 +1,17 @@
+#
+# Benjamin Nicholson / Shelby Sanchez-Herrera
+# 12/05/2021
+# Student module for group 4 project
+#
 
+
+def add_course(id, c_roster, c_max_size):
+    ...
+
+
+def drop_course(id, c_roster):
+    ...
+
+
+def list_courses(id, c_roster):
+    ...
