a2d624c31aa508d5518201ff754b5c3e24ceea0a
Benjamin Nicholson <benicholson@my.waketech.edu>
2021-11-18 13:18:17 -0500

    Function call list for admin menu.

---
+++ admin.py
@@ -0,0 +1,21 @@
+#
+# Benjamin Nicholson / Shelby Sanchez-Herrera
+# 12/05/2021
+# Admin module for group 4 project
+#
+
+
+def create_course():
+    ...
+
+
+def delete_course():
+    ...
+
+
+def drop_student():
+    ...
+
+
+def delete_student():
+    ...
